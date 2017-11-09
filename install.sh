ec30e268a93cee4f01392177d8e5b7adbb744447
Sam Pringle <15springle@gmail.com>
2017-11-09 10:13:02 -0800

    Add git pretty graphs

--- install.sh
+++ install.sh
@@ -14,3 +14,6 @@ bash -c "vim +PluginInstall +qall"
 
 # Build YCM
 bash -c "cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer"
+
+# Add pretty git graphs while we're here
+git config --global alias.hist "log --graph --date-order --date=short --pretty=format:'%C(auto)%h%d %C(reset)%s %C(bold blue)%ce %C(reset)%C(green)%cr (%cd)'"
